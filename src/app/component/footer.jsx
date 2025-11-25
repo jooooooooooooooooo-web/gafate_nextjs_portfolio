5c853ffe9bd219d3f376f6555aec8daa286b937c
JGafate <jbasga07@gmail.com>
2025-11-25 23:28:37 +0800

    formatting

--- src/app/component/footer.jsx
+++ src/app/component/footer.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 const RootFooter = ()=>{
-    return <footer className="bg-[#1c1c2d] flex p-10 justify-center items-center text-white font-bold">
+    return <footer className="bg-[#1c1c2d] flex p-6 justify-center items-center text-white font-bold">
         <p>© 2024 Jopfel Basan Gafate. All rights reserved.</p>
-        <div className="flex space-x-4 p-12">
+        <div className="flex space-x-4 p-6">
     <Link href="#top">Back to top</Link>
     <Link href="https://drive.google.com/file/d/1bwaKp3t3Zs2wdS8vTwOM0VCjB91g06ji/view?usp=sharing" className="underline">
             Resume</Link>
